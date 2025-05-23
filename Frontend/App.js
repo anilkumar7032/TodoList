e3682752d58cb683428c4d04b10df39f539a37bd
Anil kumar <h.anilkumar5478@gmail.com>
2025-05-23 13:11:13 +0530

    Add files via upload
    code

---
+++ Frontend/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddTodoForm from './component/AddTodoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AddTodoForm></AddTodoForm>
+    </div>
+  );
+}
+
+export default App;
